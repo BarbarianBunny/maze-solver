107efbb766d347c91851504964f7877353defcfd
BarbarianBunny <carury@gmail.com>
2025-01-16 15:38:51 -0800

    add line draw

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from window import Window
+from shapes import Line, Point
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(200,200), Point(600,400))
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 main()
--- src/window.py
+++ src/window.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from shapes import Line
 
 class Window:
     def __init__(self, width, height):
@@ -20,3 +21,6 @@ class Window:
 
     def close(self):
         self.is_running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
