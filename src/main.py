4204f2e6428a5e8fe2cb308f125abef3768cdc0b
BarbarianBunny <carury@gmail.com>
2025-01-16 23:52:25 -0800

    add Cell class

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from window import Window
-from shapes import Line, Point
+from shapes import Line, Point, Cell
 
 def main():
     win = Window(800, 600)
     line = Line(Point(200,200), Point(600,400))
     win.draw_line(line, "black")
+    cell = Cell(win, Point(200,200), Point(600,400))
+    cell.draw()
+
     win.wait_for_close()
+
+
 
 main()
--- src/shapes.py
+++ src/shapes.py
@@ -12,3 +12,29 @@ class Line:
 
     def draw(self, canvas, fill_color):
         canvas.create_line(self.point_1.x, self.point_1.y, self.point_2.x, self.point_2.y, fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self, window, top_left_point, bottom_right_point):
+        self._win = window
+        self._tl_point = top_left_point
+        self._br_point = bottom_right_point
+        self._tr_point = Point(self._br_point.x, self._tl_point.y)
+        self._bl_point = Point(self._tl_point.x, self._br_point.y)
+        self.has_bottom_wall = True
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+
+    def draw(self):        
+        if self.has_bottom_wall:
+            line = Line(self._bl_point, self._br_point)
+            line.draw(self._win.canvas, "black")
+        if self.has_left_wall:
+            line = Line(self._tl_point, self._bl_point)
+            line.draw(self._win.canvas, "black")
+        if self.has_right_wall:
+            line = Line(self._tr_point, self._br_point)
+            line.draw(self._win.canvas, "black")
+        if self.has_top_wall:
+            line = Line(self._tl_point, self._tr_point)
+            line.draw(self._win.canvas, "black")
